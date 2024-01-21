35a6c2932a493073e1401cae54d3c938510dfc2c
halrobison <halrobisoncreative@gmail.com>
2024-01-21 16:38:38 -0600

    part 2 completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,9 +10,21 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = " ";
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+"Who was the first American woman in space? ", 
+"True or false: 5 kilometer == 5000 meters? ",
+"(5 + 3)/2 * 10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+"Sally Ride", 
+"true", 
+"40", 
+"Trajectory", 
+"3"
+];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -27,18 +39,21 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   const input = require('readline-sync');
 
-  candidateAnswer = input.question(question);
+ for (let i = 0; i < questions.length; i++) {
+  candidateAnswers[i] = input.question(questions[i]);
+ }
   //The user enters their answer.
 }
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  console.log("Correct answer: " + correctAnswer)
-  if (candidateAnswer === correctAnswer) {
-    console.log("You were correct!"); 
-  } else {
-    console.log("You were incorrect."); 
+  //TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  for (let i = 0; i < questions.length; i++) {
+    if (candidateAnswers[i] === correctAnswers[i]) {
+      console.log(`Your answer to question ${i+1} is correct! You said ${candidateAnswers[i]} and the correct answer is ${correctAnswers[i]}.`);
+    } else {
+      console.log(`Your answer to question ${i+1} is incorrect. You said ${candidateAnswers[i]} and the correct answer is ${correctAnswers[i]}.`)
+    }
   }
 
 
@@ -52,7 +67,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello, " + candidateName);
+   console.log("Hello, " + candidateName + "! Welcome to trivia.");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
